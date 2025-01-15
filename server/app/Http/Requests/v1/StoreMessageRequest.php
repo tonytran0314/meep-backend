77b06a1dacd540ca988f719b08a6ee7f77c0cd47
Trần Gia Huy <tonytran0314@gmail.com>
2025-01-14 21:33:56 -0500

    create send message request

---
+++ server/app/Http/Requests/v1/StoreMessageRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\v1;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreMessageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'room_id' => ['required', 'exists:rooms,id'],
+            'user_id' => [
+                'required',
+                'exists:users,id',
+                Rule::exists('room_user', 'user_id')->where('room_id', $this->room_id)
+            ],
+            'content' => ['required', 'string', 'max:65535'],
+        ];
+    }
+}
