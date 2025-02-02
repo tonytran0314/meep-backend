f576af4850f25430b2f0d16bd64f372535ee6c23
Trần Gia Huy <tonytran0314@gmail.com>
2025-02-02 03:33:08 -0500

    create notifications and types table

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             RoomSeeder::class,
             RoomUserSeeder::class,
+            NotificationTypesSeeder::class,
         ]);
     }
 }
