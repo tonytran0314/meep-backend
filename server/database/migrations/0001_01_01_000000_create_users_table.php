a30f75706532b646f339ab4625fa8236fdb971ad
Huy Tran <tonytran0314@gmail.com>
2025-03-13 19:04:41 -0400

    add avatar and background col

--- server/database/migrations/0001_01_01_000000_create_users_table.php
+++ server/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('avatar')->default('meepx_default_avatar.jpg');
+            $table->string('background')->default('meepx_default_background.jpg');
             $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
